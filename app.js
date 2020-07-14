9ef56f26579e37e119df5e128979fa3e46d32968
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-07-14 18:33:39 -0500

    end of lesson 1

---
+++ app.js
@@ -0,0 +1,27 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+
+
+
+
+
+const printProfileData = profileDataArr => { // If only one parameter, you can actually omit the parentheses.
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log("================");
+
+  profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+
+printProfileData(profileDataArgs);
+
+
+
+
+
+//  • | • N O T E S • | • // 
+// • Although one cannot reassign the value of a 'const', one can edit the contents of an object or array that have been designated as a const. ;
