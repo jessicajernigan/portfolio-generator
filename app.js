e19c591600df7bf55b9c33a9ea718e0c40516ccb
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-07-16 17:25:18 -0500

    added initial inquirer functionality

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const [name, github] = profileDataArgs;
+const inquirer = require('inquirer');
+
+inquirer 
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const fs = require('fs');
+
+// const generatePage = require('./src/page-template.js');
+
+// const [name, github] = profileDataArgs;
+
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
