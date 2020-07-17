dce80c6cf53fa98b573c47387441a94dd890e941
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-07-17 11:18:17 -0500

    finished multi-project addition functionality

--- app.js
+++ app.js
@@ -20,7 +20,11 @@ const promptUser = () => {
   ]);
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
+
   console.log(`
 =================
 Add a New Project
@@ -60,15 +64,21 @@ Add a New Project
       message: 'Would you like to enter another project?',
       default: false
     }
-  ]);
+  ]).then(projectData => {
+    portfolioData.projects.push(projectData);
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+    } else {
+      return portfolioData;
+    }
+  });
 };
 
 promptUser()
-  .then(answers => console.log(answers))
   .then(promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
-
-
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
 
 // const fs = require('fs');
 
